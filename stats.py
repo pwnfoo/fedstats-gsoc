9027a04086f6506c0a39a6a300ceab3e796e9b37
Sachin Kamath <sskamath96@gmail.com>
2016-05-25 17:30:22 +0530

    Wrapped up values inside class

--- stats.py
+++ stats.py
@@ -6,6 +6,21 @@ import os
 import urllib
 
 
+class stats:
+    def __init__(self):
+        self.values = dict()
+        self.values['user'] = None
+        self.values['delta'] = 604800
+        self.values['rows_per_page'] = 100
+        self.values['not_category'] = 'meetbot'
+        self.baseurl = "https://apps.fedoraproject.org/datagrepper/raw"
+
+    def return_url(self):
+        data = urllib.urlencode(self.values)
+        full_url = self.baseurl + '?' + data
+        return full_url
+
+
 def dependency_check():
     # Without fedmsg-meta, the program will not display the human readable log
     return_val = os.system('rpm -q python2-fedmsg-meta-fedora-infrastructure >> \
@@ -19,12 +34,6 @@ def dependency_check():
 
 
 def main():
-    # Constants
-    baseurl = "https://apps.fedoraproject.org/datagrepper/raw"
-    week = 604800
-
-    # Initial invokes
-    values = dict()
 
     # fedmsg config
     config = fedmsg.config.load_config()
@@ -33,30 +42,22 @@ def main():
     # Argument Parser initialization
     parser = argparse.ArgumentParser(description='Fedora GSoC stats gatherer')
     parser.add_argument('--user', '-u', help='FAS username', required=True)
-    parser.add_argument('--time', '-t', help='Time in weeks', default=1)
+    parser.add_argument('--weeks', '-w', help='Time in weeks', default=1)
     args = parser.parse_args()
 
-    values['user'] = args.user
-    values['delta'] = int(args.time) * week
-    values['rows_per_page'] = 100
-    values['not_category'] = 'meetbot'
+    userstats = stats()
+    userstats.values['user'] = args.user
+    userstats.values['delta'] = int(args.weeks) * 604800
 
-    '''
-    values = {
-        'user': 'skamath',
-        'delta': '500000',
-        'topic': 'org.fedoraproject.prod.wiki.article.edit'
-    }
-    '''
-    data = urllib.urlencode(values)
-    full_url = baseurl + '?' + data
-    print full_url
+    full_url = userstats.return_url()
     response = urllib.urlopen(full_url)
     raw_json = response.read()
     unicode_json = json.loads(raw_json)
 
     for activity in unicode_json['raw_messages']:
         print fedmsg.meta.msg2repr(activity)
+
+
 if __name__ == '__main__':
     if(dependency_check()):
         main()
