1c7d2859d285a1140c2d08a0108a086cde142ae0
Sachin Kamath <sskamath96@gmail.com>
2016-06-15 00:50:53 +0530

    Code cleanup and added pycon_us script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
+from termcolor import colored
 from six.moves import input
-
 import os
 import fedmsg
 import argparse
@@ -9,9 +9,6 @@ import calendar
 import fedmsg.meta
 import stats
 import output
-from termcolor import colored
-
-
 
 
 def main():
@@ -19,20 +16,21 @@ def main():
     config = fedmsg.config.load_config()
     fedmsg.meta.make_processors(**config)
 
-    #Initializing to None to prevent errors while generating multiple reports
+    # Initializing to None to prevent errors while generating multiple reports
     stats.unicode_json = {}
     i_count = 0
 
     # Argument Parser initialization
-    parser = argparse.ArgumentParser(description='Summer Coding stats gatherer')
+    parser = argparse.ArgumentParser(description='Fedora Statistics Gatherer')
     parser.add_argument('--user', '-u', help='FAS username')
-    parser.add_argument('--weeks','-w', help='Time in weeks', default=1)
+    parser.add_argument('--weeks', '-w', help='Time in weeks', default=1)
     parser.add_argument('--mode', '-m', help="Type of Output", default='text')
     parser.add_argument('--output', '-o', help="Output name", default='stats')
-    parser.add_argument('--category', '-c', help="Category for graphs", default=None)
+    parser.add_argument('--category', '-c', help="Sub Category", default='')
     parser.add_argument('--start', '-s', help="Start Date", default='')
-    parser.add_argument('--end','-e', help = "End Date", default='' )
-    parser.add_argument('--interactive', '-i', help="Enable interactive mode", action='store_true')
+    parser.add_argument('--end', '-e', help="End Date", default='')
+    parser.add_argument('--interactive', '-i', help="Enable interactive mode",
+                        action='store_true')
     args = parser.parse_args()
     args.output = args.user
 
@@ -40,7 +38,7 @@ def main():
     if args.interactive:
         stats.values['user'] = str(input("Enter FAS Username : ")).lower()
         stats.weeks = int(args.weeks)
-        stats.values['delta'] = 604800 * int(input("Number of weeks stats required for : "))
+        stats.values['delta'] = 604800 * int(input("Number of weeks : "))
         stats.category = str(input("Enter category : ")).lower()
         output.mode = str(input("Type of output : ")).lower()
         output.filename = str(input("Output file : ")).lower()
@@ -54,48 +52,52 @@ def main():
     else:
         stats.values['user'] = str(args.user).lower()
         stats.values['delta'] = int(args.weeks) * 604800
-        stats.category = args.category
+        stats.category = args.category.lower()
         stats.start = args.start
         stats.end = args.end
         stats.weeks = int(args.weeks)
         output.mode = args.mode.lower()
         output.filename = args.output.lower()
-    # For json and text output, we need the JSON rather than the categories
+    # For png and SVG, we need a drawable object to be called
     if output.mode in ['png', 'svg', 'csv']:
         # Draw object for the above mentioned categories.
         draw_obj = stats.return_categories()
 
-        # To handle user with no activity
-        if len(draw_obj) ==  0:
-            print (colored("[!] ", 'red') + 'No activity found for user ' + args.user)
+        # To handle user with no activity; TO-DO -> Make this a function
+        if len(draw_obj) == 0:
+            print (colored("[!] ", 'red') + 'No activity found for user' +
+                    args.user)
             return 1
 
         # Generate the output graph objects required for calling generate_graph
-        output.generate_graph(draw_obj, "Topic distribution of " + stats.values['user'], None, 'pie')
+        output.generate_graph(draw_obj, "Topic distribution of " +
+                                        stats.values['user'], None, 'pie')
         draw_obj2 = stats.return_subcategories(stats.category)
         interactions = stats.return_interactions(draw_obj2)
 
-        # Check if a category input was given and generate the specific category graph
-        if stats.category != '' :
-            output.generate_graph(draw_obj2, "Category: " + stats.category.capitalize()\
-            + "\nUser: " + args.user, stats.category, 'bar')
+        # Check if a category input was given and generate category graph
+        if stats.category != '':
+            output.generate_graph(draw_obj2, "Category: " + stats.category +
+                                "\nUser: " + args.user, stats.category, 'bar')
 
         # Check if the sub-sub-category exists and generate it's graph
-        if not None in list(interactions.keys()):
+        if None not in list(interactions.keys()):
             for keys in interactions:
                 i_count += 1
-                output.generate_graph(interactions[keys], "Interaction with "+str(keys)+"\nCategory:  "\
-                + stats.category.capitalize(), stats.category +"_" + keys, 'pie')
+                output.generate_graph(interactions[keys], "Interaction with "
+                        + str(keys)+"\nCategory:  " + stats.category.capitalize(),
+                        stats.category + "_" + keys, 'pie')
 
-    # If not image based outputs, check if the input matches any of the text based inputs
-    elif output.mode in ['json','text','markdown']:
+    # If not image, check if the input matches any of the text based inputs
+    elif output.mode in ['json', 'text', 'markdown']:
         draw_obj = stats.return_json()
+
         # To handle user with no activity
         if draw_obj['total'] == 0:
-            print (colored("[!] ", 'red') + 'No activity found for user ' + str(args.user))
+            print (colored("[!] ", 'red') + 'No activity found for user ' +
+                        str(args.user))
             return 1
         output.generate_graph(draw_obj, args.user)
-
 
 
 if __name__ == '__main__':
--- output.py
+++ output.py
@@ -18,22 +18,25 @@ csv_init = False
 mode = 'text'
 cat = None
 
+
 # Gets a drawable object argument and renders an SVG Image of it.
 def draw_svg(graph_obj):
-    if cat is None :
+    if cat is None:
         fname = filename + '_main' + '.svg'
-    else :
+    else:
         fname = filename + "_" + cat + '.svg'
     graph_obj.render_to_file(fname)
     os.system("firefox " + fname)
 
+
 # Gets a drawable object argument and renders a PNG image of it.
 def draw_png(graph_obj):
-    if cat is None :
+    if cat is None:
         fname = filename + '_main' + '.png'
-    else :
+    else:
         fname = filename + "_" + cat + '.png'
-    graph_obj.render_to_png(fname)
+        graph_obj.render_to_png(fname)
+
 
 # Generates a drawable pie chart object from a dictionary passed.
 def draw_pie(output_json, title):
@@ -44,6 +47,7 @@ def draw_pie(output_json, title):
         pie_chart.add(str(key), round(percent, 2))
     return pie_chart
 
+
 # Generates a drawable pie chart object from a dictionary passed.
 def draw_bar(output_json, title):
     bar_chart = pygal.Bar(width=500, height=500)
@@ -52,31 +56,33 @@ def draw_bar(output_json, title):
         bar_chart.add(str(key), output_json[key])
     return bar_chart
 
+
 # Generates CSV report for the user from the dictionary passed
 def save_csv(output_json):
-    global csv_init,cat
+    global csv_init, cat
     fname = filename + '_main.csv'
     fout = open(fname, 'a')
     csvw = csv.writer(fout)
 
     # Write the dates into CSV
-    if not csv_init :
-        csvw.writerows  (
-                            [['Start Date : ', stats.start],
-                            ['End Date  : ', stats.end],
-                            ['']]
-                        )
+    if not csv_init:
+        csvw.writerows(
+            [['Start Date : ', stats.start],
+                ['End Date  : ', stats.end],
+                ['']])
         csv_init = True
 
     # Initial heading row
-    data = [['Username','Category', 'Activity Count', 'Percentage'],[]]
+    data = [['Username', 'Category', 'Activity Count', 'Percentage'], []]
     for key in output_json:
-        percent = round(output_json[key] / float(sum(output_json.values())) * 100, 2)
+        percent = round(output_json[key] / float(sum(output_json.values())) *
+                        100, 2)
         if cat is not None and cat.capitalize() != key.capitalize():
-            data.append([stats.values['user'], cat.capitalize() + "." + \
+            data.append([stats.values['user'], cat.capitalize() + "." +
                         key.capitalize(), output_json[key], str(percent)+'%'])
         else:
-            data.append([stats.values['user'], key.capitalize(), output_json[key], str(percent)+'%'])
+            data.append([stats.values['user'], key.capitalize(),
+                        output_json[key], str(percent)+'%'])
 
     # Insert blank lines and total
     data.append([''])
@@ -84,6 +90,7 @@ def save_csv(output_json):
     data.append([''])
     csvw.writerows(data)
     fout.close()
+
 
 # Saves category-wise text report of a user.
 def save_text(unicode_json):
@@ -100,13 +107,14 @@ def save_text(unicode_json):
                 actcount += 1
                 # Print the category once
                 if flag is True:
-                    fout.write("\n\n** Category : "+category.capitalize()+" **\n")
+                    fout.write("\n\n** Category : "+category.capitalize() + " **\n")
                     flag = False
                 fout.write("* "+fedmsg.meta.msg2subtitle(activity)+"\n")
         fout.write("\nTotal Entries in category : " + str(actcount) + "\n")
-        fout.write("\nPercentage participation in category : " + \
-                            str(round(100*actcount/float(unicode_json['total']),2)) + "\n")
+        fout.write("\nPercentage participation in category : " +
+                    str(round(100*actcount/float(unicode_json['total']),2)) + "\n")
     fout.close()
+
 
 # Saves the markdown version of the text log
 def save_markdown(unicode_json):
@@ -126,14 +134,16 @@ def save_markdown(unicode_json):
                     flag = False
                 fout.write("* "+fedmsg.meta.msg2subtitle(activity)+"\n")
         fout.write("\n* **Total Entries in category :** " + str(actcount) + "\n")
-        fout.write("\n* **Percentage participation in category :** " + \
-                            str(round(100*actcount/float(unicode_json['total']),2)) + "\n")
+        fout.write("\n* **Percentage participation in category :** " +
+                    str(round(100*actcount/float(unicode_json['total']), 2)) + "\n")
     fout.close()
     render_report(fname)
+
 
 # WIP - Renders HTML from markdown, invoked my markdown function atm.
 def render_report(fname):
     grip.export(fname, title="Summer Coding Statistics")
+
 
 # Saves the JSON as a file.
 def save_json(unicode_json):
@@ -144,7 +154,8 @@ def save_json(unicode_json):
     except IOError:
         print("[!] Could not write into directory. Check Permissions")
 
-# Identifies categories, and generates drawable / file objects for all the above functions.
+
+# Identifies categories & generates drawable objects for the above functions.
 def generate_graph(output_json, title, category=None, gtype=None):
     global cat
     cat = category
